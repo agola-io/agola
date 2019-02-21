66f27f224857060a548e90376a84c85a54109bba
Simone Gotti <simone.gotti@gmail.com>
2019-02-21 16:32:27 +0100

    Add slice utils

---
+++ internal/util/string.go
@@ -0,0 +1,47 @@
+// This file is part of Agola
+//
+// Copyright (C) 2019 Sorint.lab
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package util
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+func CountLines(s string) (uint, error) {
+	count := uint(0)
+
+	// use a reader instead of a scanner
+	br := bufio.NewReader(strings.NewReader(s))
+
+	stop := false
+	for {
+		if stop {
+			break
+		}
+		_, err := br.ReadBytes('\n')
+		if err != nil {
+			if err != io.EOF {
+				return 0, err
+			}
+			stop = true
+		}
+		count++
+	}
+	return count, nil
+}
