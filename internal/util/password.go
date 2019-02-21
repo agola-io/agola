7c694488ba454b301c3ab16f20e75cdfa68d0002
Simone Gotti <simone.gotti@gmail.com>
2019-02-21 16:04:00 +0100

    Add password utils

---
+++ internal/util/password.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func PasswordHash(password string) (string, error) {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(passwordHash), err
+}
+
+func CompareHashAndPassword(passwordHash, password string) (bool, error) {
+	if err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password)); err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
