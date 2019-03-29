b5afa5eb91cc65681821d2ec9b7549b3c9647577
Simone Gotti <simone.gotti@gmail.com>
2019-03-29 12:15:48 +0100

    runservice: rework store and readdb logic
    * Remove all the small index files on the lts
    * Keep on s3 only a full index of all runs containing the runid, grouppath and phase
      million of runs can take only some hundred of megabytes
    * Periodically create a new dump of the index

--- internal/services/runservice/scheduler/common/common.go
+++ internal/services/runservice/scheduler/common/common.go
@@ -46,6 +46,8 @@ var (
 
 	EtcdExecutorsDir = "executors"
 	EtcdTasksDir     = "tasks"
+
+	EtcdLastIndexKey = "lastindex"
 )
 
 func EtcdRunKey(runID string) string       { return path.Join(EtcdRunsDir, runID) }
