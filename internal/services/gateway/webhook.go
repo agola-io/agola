58a77ad2212ba7bd33cbdbb2df36a63d69f1dcbc
Simone Gotti <simone.gotti@gmail.com>
2019-04-03 17:05:50 +0200

    gateway: handle skipped webhook

--- internal/services/gateway/webhook.go
+++ internal/services/gateway/webhook.go
@@ -174,6 +174,13 @@ func (h *webhooksHandler) handleWebhook(r *http.Request) (int, string, error) {
 		if err != nil {
 			return http.StatusBadRequest, "", errors.Wrapf(err, "failed to parse webhook")
 		}
+		// skip nil webhook data
+		// TODO(sgotti) report the reason of the skip
+		if webhookData == nil {
+			h.log.Infof("skipping webhook")
+			return 0, "", nil
+		}
+
 		webhookData.ProjectID = projectID
 
 		// get project variables
@@ -222,6 +229,12 @@ func (h *webhooksHandler) handleWebhook(r *http.Request) (int, string, error) {
 		webhookData, err = gitSource.ParseWebhook(r)
 		if err != nil {
 			return http.StatusBadRequest, "", errors.Wrapf(err, "failed to parse webhook")
+		}
+		// skip nil webhook data
+		// TODO(sgotti) report the reason of the skip
+		if webhookData == nil {
+			h.log.Infof("skipping webhook")
+			return 0, "", nil
 		}
 
 		user, _, err := h.configstoreClient.GetUser(ctx, userID)
