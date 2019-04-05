f4aaba0bce387a9d8ec24b38cc439ba06058b4d6
Simone Gotti <simone.gotti@gmail.com>
2019-04-05 11:11:27 +0200

    cmd: implement variable delete

---
+++ cmd/agola/cmd/projectgroupvariabledelete.go
@@ -0,0 +1,44 @@
+// This file is part of Agola
+//
+// Copyright (C) 2019 Sorint.lab
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var cmdProjectGroupVariableDelete = &cobra.Command{
+	Use:   "delete",
+	Short: "delete a variable",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := variableDelete(cmd, "projectgroup", args); err != nil {
+			log.Fatalf("err: %v", err)
+		}
+	},
+}
+
+func init() {
+	flags := cmdProjectGroupVariableDelete.Flags()
+
+	flags.StringVar(&variableDeleteOpts.parentRef, "projectgroup", "", "project group id or full path)")
+	flags.StringVarP(&variableDeleteOpts.name, "name", "n", "", "variable name")
+
+	cmdProjectGroupVariableDelete.MarkFlagRequired("projectgroup")
+	cmdProjectGroupVariableDelete.MarkFlagRequired("name")
+
+	cmdProjectGroupVariable.AddCommand(cmdProjectGroupVariableDelete)
+}
---
+++ cmd/agola/cmd/projectvariabledelete.go
@@ -0,0 +1,79 @@
+// This file is part of Agola
+//
+// Copyright (C) 2019 Sorint.lab
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"context"
+
+	"github.com/sorintlab/agola/internal/services/gateway/api"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var cmdProjectVariableDelete = &cobra.Command{
+	Use:   "delete",
+	Short: "delete a variable",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := variableDelete(cmd, "project", args); err != nil {
+			log.Fatalf("err: %v", err)
+		}
+	},
+}
+
+type variableDeleteOptions struct {
+	parentRef string
+	name      string
+}
+
+var variableDeleteOpts variableDeleteOptions
+
+func init() {
+	flags := cmdProjectVariableDelete.Flags()
+
+	flags.StringVar(&variableDeleteOpts.parentRef, "project", "", "project id or full path)")
+	flags.StringVarP(&variableDeleteOpts.name, "name", "n", "", "variable name")
+
+	cmdProjectVariableDelete.MarkFlagRequired("projectgroup")
+	cmdProjectVariableDelete.MarkFlagRequired("name")
+
+	cmdProjectVariable.AddCommand(cmdProjectVariableDelete)
+}
+
+func variableDelete(cmd *cobra.Command, ownertype string, args []string) error {
+	gwclient := api.NewClient(gatewayURL, token)
+
+	switch ownertype {
+	case "project":
+		log.Infof("deleting project variable")
+		_, err := gwclient.DeleteProjectVariable(context.TODO(), variableDeleteOpts.parentRef, variableDeleteOpts.name)
+		if err != nil {
+			return errors.Wrapf(err, "failed to delete project variable")
+		}
+		log.Infof("project variable deleted")
+	case "projectgroup":
+		log.Infof("deleting project group variable")
+		_, err := gwclient.DeleteProjectGroupVariable(context.TODO(), variableDeleteOpts.parentRef, variableDeleteOpts.name)
+		if err != nil {
+			return errors.Wrapf(err, "failed to delete project group variable")
+		}
+		log.Infof("project group variable deleted")
+	}
+
+	return nil
+}
