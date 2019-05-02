87de815a50a11f2ff861f6259a8c0031613a39b4
Simone Gotti <simone.gotti@gmail.com>
2019-05-02 23:40:28 +0200

    validation: make uuid like names not valid
    names that are valid uuids are not valid. This is needed to accept both names or
    uuid in rest APIs without using a special syntax to distinguish them

--- internal/util/validation_test.go
+++ internal/util/validation_test.go
@@ -11,6 +11,10 @@ var (
 		"foo-1",
 		"foo-1-bar",
 		"f12oo-bar33",
+		"cba7b810-9dad-11d1-80b4-00c04fd430c",
+		"cba7b810-9dad-11d1-80b4000c04fd430c8",
+		"cba7b8109dad11d180b400c04fd430c89",
+		"cba7b8109dad11d180b400c04fd430c",
 	}
 	badNames = []string{
 		"",
@@ -26,6 +30,10 @@ var (
 		"foo_bar",
 		"foo#bar",
 		"1foobar",
+		"cba7b810-9dad-11d1-80b4-00c04fd430c8",
+		"{cba7b810-9dad-11d1-80b4-00c04fd430c8}",
+		"urn:uuid:cba7b810-9dad-11d1-80b4-00c04fd430c8",
+		"cba7b8109dad11d180b400c04fd430c8",
 	}
 )
 
