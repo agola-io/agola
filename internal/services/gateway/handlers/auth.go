d28c023a68bb1521135fb185450f764d5706c15f
Simone Gotti <simone.gotti@gmail.com>
2019-05-05 17:30:38 +0200

    gateway: set user admin value in context

--- internal/services/gateway/handlers/auth.go
+++ internal/services/gateway/handlers/auth.go
@@ -79,6 +79,11 @@ func (h *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			// pass userid to handlers via context
 			ctx = context.WithValue(ctx, "userid", user.ID)
 			ctx = context.WithValue(ctx, "username", user.Name)
+
+			if user.Admin {
+				ctx = context.WithValue(ctx, "admin", true)
+			}
+
 			h.next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
@@ -125,9 +130,14 @@ func (h *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		// pass userid to handlers via context
+		// pass userid and username to handlers via context
 		ctx = context.WithValue(ctx, "userid", user.ID)
 		ctx = context.WithValue(ctx, "username", user.Name)
+
+		if user.Admin {
+			ctx = context.WithValue(ctx, "admin", true)
+		}
+
 		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
 	}
