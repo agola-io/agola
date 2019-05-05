84e15f8ad0cc521e55855e527af059e36e613713
Simone Gotti <simone.gotti@gmail.com>
2019-05-05 14:27:22 +0200

    gateway: move remaining project(group) logic from api to actions

--- internal/services/gateway/action/project.go
+++ internal/services/gateway/action/project.go
@@ -31,6 +31,14 @@ import (
 	"github.com/pkg/errors"
 )
 
+func (h *ActionHandler) GetProject(ctx context.Context, projectRef string) (*csapi.Project, error) {
+	project, resp, err := h.configstoreClient.GetProject(ctx, projectRef)
+	if err != nil {
+		return nil, ErrFromRemote(resp, err)
+	}
+	return project, nil
+}
+
 type CreateProjectRequest struct {
 	CurrentUserID       string
 	Name                string
@@ -202,4 +210,12 @@ func (h *ActionHandler) ReconfigProject(ctx context.Context, projectRef string) 
 	// TODO(sgotti) update project repo path if the remote let us query by repository id
 
 	return h.SetupProject(ctx, rs, user, la, p)
+}
+
+func (h *ActionHandler) DeleteProject(ctx context.Context, projectRef string) error {
+	resp, err := h.configstoreClient.DeleteProject(ctx, projectRef)
+	if err != nil {
+		return ErrFromRemote(resp, err)
+	}
+	return nil
 }
