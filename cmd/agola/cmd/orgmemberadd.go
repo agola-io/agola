da70fc7c3a8c5c5ddbbda19d1718869ba0755a5a
Simone Gotti <simone.gotti@gmail.com>
2019-05-14 11:25:12 +0200

    cmd: add org member add/remove commands

---
+++ cmd/agola/cmd/orgmemberadd.go
@@ -0,0 +1,71 @@
+// This file is part of Agola
+//
+// Copyright (C) 2019 Sorint.lab
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/sorintlab/agola/internal/services/gateway/api"
+	"github.com/sorintlab/agola/internal/services/types"
+
+	"github.com/spf13/cobra"
+)
+
+var cmdOrgMemberAdd = &cobra.Command{
+	Use:   "add",
+	Short: "adds or updates an organization member",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := orgMemberAdd(cmd, args); err != nil {
+			log.Fatalf("err: %v", err)
+		}
+	},
+}
+
+type orgMemberAddOptions struct {
+	orgname  string
+	username string
+	role     string
+}
+
+var orgMemberAddOpts orgMemberAddOptions
+
+func init() {
+	flags := cmdOrgMemberAdd.Flags()
+
+	flags.StringVarP(&orgMemberAddOpts.orgname, "orgname", "n", "", "organization name")
+	flags.StringVar(&orgMemberAddOpts.username, "username", "", "user name")
+	flags.StringVarP(&orgMemberAddOpts.role, "role", "r", "member", "member role (owner or member)")
+
+	cmdOrgMemberAdd.MarkFlagRequired("orgname")
+	cmdOrgMemberAdd.MarkFlagRequired("username")
+
+	cmdOrgMember.AddCommand(cmdOrgMemberAdd)
+}
+
+func orgMemberAdd(cmd *cobra.Command, args []string) error {
+	gwclient := api.NewClient(gatewayURL, token)
+
+	log.Infof("adding/updating member %q to organization %q with role %q", orgMemberAddOpts.username, orgMemberAddOpts.orgname, orgMemberAddOpts.role)
+	_, _, err := gwclient.AddOrgMember(context.TODO(), orgMemberAddOpts.orgname, orgMemberAddOpts.username, types.MemberRole(orgMemberAddOpts.role))
+	if err != nil {
+		return errors.Wrapf(err, "failed to add/update organization member")
+	}
+
+	return nil
+}
---
+++ cmd/agola/cmd/orgmemberremove.go
@@ -0,0 +1,68 @@
+// This file is part of Agola
+//
+// Copyright (C) 2019 Sorint.lab
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/sorintlab/agola/internal/services/gateway/api"
+
+	"github.com/spf13/cobra"
+)
+
+var cmdOrgMemberRemove = &cobra.Command{
+	Use:   "remove",
+	Short: "removes an organization member",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := orgMemberRemove(cmd, args); err != nil {
+			log.Fatalf("err: %v", err)
+		}
+	},
+}
+
+type orgMemberRemoveOptions struct {
+	orgname  string
+	username string
+}
+
+var orgMemberRemoveOpts orgMemberRemoveOptions
+
+func init() {
+	flags := cmdOrgMemberRemove.Flags()
+
+	flags.StringVarP(&orgMemberRemoveOpts.orgname, "orgname", "n", "", "organization name")
+	flags.StringVar(&orgMemberRemoveOpts.username, "username", "", "user name")
+
+	cmdOrgMemberRemove.MarkFlagRequired("orgname")
+	cmdOrgMemberRemove.MarkFlagRequired("username")
+
+	cmdOrgMember.AddCommand(cmdOrgMemberRemove)
+}
+
+func orgMemberRemove(cmd *cobra.Command, args []string) error {
+	gwclient := api.NewClient(gatewayURL, token)
+
+	log.Infof("removing member %q from organization %q", orgMemberRemoveOpts.username, orgMemberRemoveOpts.orgname)
+	_, err := gwclient.RemoveOrgMember(context.TODO(), orgMemberRemoveOpts.orgname, orgMemberRemoveOpts.username)
+	if err != nil {
+		return errors.Wrapf(err, "failed to remove organization member")
+	}
+
+	return nil
+}
