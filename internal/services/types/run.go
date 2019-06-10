90cc6e971e78f2543f709ad413600c44a1e8e06a
Simone Gotti <simone.gotti@gmail.com>
2019-06-10 16:49:39 +0200

    webhook: add runRefType and convert webhook event
    Introduce a runRefType that represent the ref type of the Run (branch/tag/PR)
    Convert the webhook event type to the runRefType and use it to generate the run
    group.

---
+++ internal/services/types/run.go
@@ -0,0 +1,33 @@
+// This file is part of Agola
+//
+// Copyright (C) 2019 Sorint.lab
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package types
+
+type RunType string
+
+const (
+	RunTypeProject RunType = "project"
+	RunTypeUser    RunType = "user"
+)
+
+type RunRefType string
+
+const (
+	RunRefTypeBranch      RunRefType = "branch"
+	RunRefTypeTag         RunRefType = "tag"
+	RunRefTypePullRequest RunRefType = "pull_request"
+)
